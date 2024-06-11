4f47735e0c850f2016df0a2906037b7f31cd4b02
최서영 <mig3023@naver.com>
2024-06-11 18:35:18 +0900

    promise

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-// console.log('seochosy');
+// console.log('seochojade');
 
 // import Cat from './oop.js';
 // export default Cat;
-export * as oop from "../oop.js";
-export * as map from "../map.js";
-export { fromNow, rand } from "./utils.js";
+export * as oop from './oop.js';
+export * as map from './map.js';
+export { fromNow, rand } from './utils.js';
