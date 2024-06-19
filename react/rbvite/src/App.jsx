c2ac0b6338685f42c12f0087bbf7feb8a63e164a
최서영 <mig3023@naver.com>
2024-06-19 11:44:22 +0900

    hello-Login

--- react/rbvite/src/App.jsx
+++ react/rbvite/src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Hello from "./components/Hello";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [didLogin, setDidLogin] = useState(true);
+  const plusCount = () => setCount(count + 1);
+
+  const toggleLogin = () => {
+    setDidLogin(!didLogin);
+  };
 
   return (
     <>
       <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+        {didLogin && <Hello name='seoy' age={25} plusCount={plusCount} />}
       </div>
-      <h1>Vite + React + seoy</h1>
+      <button onClick={toggleLogin}>
+        Toggle{didLogin ? "Logined" : "Not Logined"}
+      </button>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
---
+++ react/rbvite/src/components/Hello.jsx
@@ -0,0 +1,12 @@
+export default function Hello(props) {
+  return (
+    <h1
+      onClick={props.plusCount}
+      style={{
+        cursor: "pointer",
+      }}
+    >
+      Hello, {props.name}! <small className='font-sm'>({props.age})</small>
+    </h1>
+  );
+}
