a59adfc5be56b4ca19cca1726cbd1472315a5ba6
seoyoung <mig3023@naver.com>
2024-06-20 11:50:59 +0900

    profile

--- react/rbvite/src/components/Hello.jsx
+++ react/rbvite/src/components/Hello.jsx
@@ -1,13 +1,7 @@
+
 export default function Hello(props) {
-  return (
-    <h1
-      onClick={props.plusCount}
-      style={{
-        cursor: "pointer",
-      }}
-    >
-      Hello, {props.name}!{" "}
-      <small className='font-sm text-red-500'>({props.age})</small>
-    </h1>
-  );
+  // console.log('Heeeeeeeeelo')
+  return <h1 onClick={props.plusCount} style={{
+  cursor: 'pointer'
+}}>Hello, {props.name}! <small className="font-sm text-red-500">({props.age + 1})</small></h1>
 }
