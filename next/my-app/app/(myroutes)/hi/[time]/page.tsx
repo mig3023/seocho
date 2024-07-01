2519236b54a3fc62562f44a198b9e3789e3f4396
seoyoung <mig3023@naver.com>
2024-07-01 17:41:10 +0900

    nested dynamic routes

--- next/my-app/app/(myroutes)/hi/[time]/page.tsx
+++ next/my-app/app/(myroutes)/hi/[time]/page.tsx
@@ -1,3 +1,6 @@
+import { redirect } from 'next/navigation';
+import { TIMES } from '@/utils/route-utils';
+
 type Params = {
   params: {
     time: string;
@@ -7,7 +10,21 @@ type Params = {
 // afternoon ==> Afternoon
 const toUpperFirstChar = (s: string) => `${s[0].toUpperCase()}${s.slice(1)}`;
 
+// server static html generating
+export async function generateStaticParams() {
+  return TIMES.map((time) => ({
+    time,
+  }));
+  // return [{ time: 'morning' }, { time: 'afternoon' }, { time: 'evening' }];
+}
+
 export default function Time({ params }: Params) {
   const { time } = params;
+  console.log('🚀  time:', time);
+  if (!TIMES.includes(time)) {
+    // return notFound();
+    redirect('/hi');
+  }
+
   return <h3 className='text-3xl'>Good {toUpperFirstChar(time)}!</h3>;
 }
